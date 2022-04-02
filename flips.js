57c1f477cd87e402ea6bdc2c0fc70b3f793e8ee3
jasonmanning <jasonman@live.unc.edu>
2022-04-01 21:43:35 -0400

    flips changes

--- flips.js
+++ flips.js
@@ -1,7 +1,11 @@
 // Import the coinFlip function from your coin.mjs file
 
-import { coinFlip } from "./modules/coin.mjs";
+import { coinFlip, countFlips } from "./modules/coin.mjs";
+import minimist from "minimist";
 
 // Call the coinFlip function and put the return into STDOUT
+const myArgs = minimist(process.argv.slice(2));
 
+var array = coinFlips(myArgs['number']);
+console.log(array);
 console.log(coinFlip());
