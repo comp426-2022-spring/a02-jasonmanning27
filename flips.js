2e10f52c57a8ffbb879dc61e87b02275a1af02d8
jasonmanning <jasonman@live.unc.edu>
2022-04-01 21:54:14 -0400

    test

--- flips.js
+++ flips.js
@@ -5,7 +5,12 @@ import minimist from "minimist";
 
 // Call the coinFlip function and put the return into STDOUT
 const myArgs = minimist(process.argv.slice(2));
+const number = args.number || 1; // need to make sure it has a value if no input
+
+let flips = coinFlips(number);
+let sum = countFlips(flips);
 
 var array = coinFlips(myArgs['number']);
-console.log(array);
-console.log(coinFlip());
+
+console.log(flips);
+console.log(sum);
