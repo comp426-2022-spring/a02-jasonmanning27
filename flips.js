45192c5f9be17042349309815fcb0c6d9e22bb44
jasonmanning <jasonman@live.unc.edu>
2022-04-04 16:38:07 -0400

    test

--- flips.js
+++ flips.js
@@ -4,8 +4,8 @@ import { coinFlip, countFlips } from "./modules/coin.mjs";
 import minimist from "minimist";
 
 // Call the coinFlip function and put the return into STDOUT
-const input = minimist(process.input.slice(2));
-const number = input.number || 1;
+const args = minimist(process.args.slice(2));
+const number = args.number || 1;
 
 let flips = coinflips(number);
 let sum = countFlips(flips);
