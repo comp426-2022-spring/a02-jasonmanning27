40f748663110e44f027d3988a6a6ed6add5adc7e
jasonmanning <jasonman@live.unc.edu>
2022-04-04 16:34:00 -0400

    test

--- flips.js
+++ flips.js
@@ -4,19 +4,12 @@ import { coinFlip, countFlips } from "./modules/coin.mjs";
 import minimist from "minimist";
 
 // Call the coinFlip function and put the return into STDOUT
-const input = minimist(process.argv.slice(2));
+const input = minimist(process.input.slice(2));
+const number = input.number || 1;
 
-input['number']; 
-var flips = []; // initialize flips array
+let flips = coinflips(number);
+let sum = countFlips(flips);
 
-if(input != null) { // if input is number
-    flips = coinFlips(number);
-    
-    console.log(flips);
-    console.log(countFlips(flips));
-} else { // if no input
-    flips = coinFlips(1); // set to 1 if no input
+console.log(flips);
+console.log(sum);
 
-    console.log(flips);
-    console.log(countFlips(flips));
-}
